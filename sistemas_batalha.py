909bcc9cd667741ff42f90172aa59b19db70c123
Erick Garantizado <garantizadoerick@gmail.com>
2021-01-25 23:19:18 -0300

    adicionado a função critico

--- sistemas_batalha.py
+++ sistemas_batalha.py
@@ -1,7 +1,10 @@
 from time import sleep
 from projetos.jogo_de_decisao import uteis
 
-def batalha():
+def batalha_txt():
+    """
+    Escreve 'batalha' como se estivesse soletrando
+    """
     print(uteis.linha2())
     print('{:>30}'.format('B'), end='')
     sleep(0.5)
@@ -18,3 +21,15 @@ def batalha():
     print(' A')
     sleep(0.5)
     print(uteis.linha2())
+
+
+def critico(dado20, tentativa):
+
+    if dado20 == 20:
+        critical = True
+        print(f'd20: {dado20} -- CRITICALL !!!')
+    else:
+        print(f'd20: {dado20}')
+    sleep(2)
+    print(f'Tentativa: {tentativa}')
+    sleep(3)
